a0a3fbc6370b261982f399d5388658f3031e6f05
Nitish Reuben <nitishreuben2600@gmail.com>
2020-02-03 17:26:36 -0800

    automated symlinking

---
+++ setup.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# make sure we have pulled in and updated any submodules
+git submodule init
+git submodule update
+
+# what directories should be installable by all users including the root user
+base=(
+    zsh
+)
+
+# folders that should, or only need to be installed for a local user
+useronly=(
+    xorg
+    compton
+    i3
+    polybar
+    termite
+    neovim
+    kakoune
+    zathura
+)
+
+# run the stow command for the passed in directory ($2) in location $1
+stowit() {
+    usr=$1
+    app=$2
+    # -v verbose
+    # -R recursive
+    # -t target
+    stow -v -R -t ${usr} ${app}
+}
+
+echo ""
+echo "Stowing apps for user: ${whoami}"
+
+# install apps available to local users and root
+for app in ${base[@]}; do
+    stowit "${HOME}" $app 
+done
+
+# install only user space folders
+for app in ${useronly[@]}; do
+    if [[! "$(whoami)" = *"root"*]]; then
+        stowit "${HOME}" $app 
+    fi
+done
+
+echo ""
+echo "##### ALL DONE"
+
